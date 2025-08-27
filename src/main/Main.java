ceb844cfc49093c63f46566ae6d5c0d3ede587bb
Rafael <rafaelmelo01111998@gmail.com>
2025-08-27 09:35:45 -0300

    nao finzalido

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,40 @@
 package main;
 import model.*;
+import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args) {
-    Guerreiro guerreiro = new Guerreiro("Artorias");
-    Mago mago = new Mago("Gandalf");
+    Scanner input = new Scanner(System.in);
 
-    while (guerreiro.estaVivo() && mago.estaVivo()){
-      guerreiro.atacar(mago);
-      if(!mago.estaVivo()){
-        break;
-      }
-      mago.atacarMagia(guerreiro);
+    System.out.println("Seja bem vindo ao Jogo mini RPG");
+    System.out.println("por favor, digite seu nome de jogador: ");
+    String nomeJogador = input.nextLine();
+
+   System.out.println("Escolha sua classe: ");
+   System.out.println("[1] Guerreiro");
+   System.out.println("[2] Mago");
+   int escolhaClasse = input.nextInt();
+
+   Personagem jogador;
+   Personagem inimigo;
+   switch (escolhaClasse) {
+    case 1:
+      jogador = new Guerreiro(nomeJogador);
+      inimigo = new Mago("Saruman");
+      break;
+    case 2:
+      jogador = new Mago(nomeJogador);
+      inimigo = new Guerreiro("Artorias");
+      break;
+    default:
+      break;
+   }
+
+    if(jogador.estaVivo()){
+      System.out.println(jogador.getNome() + "venceu");
+    } else{
+      System.out.println(inimigo.getNome() + "venceu");
     }
-
-      if(guerreiro.estaVivo()){
-        System.out.println(guerreiro.getNome() + "venceu");
-      } else{
-        System.out.println(mago.getNome() + "venceu");
-      }
-    
+    input.close();
   }
 }
